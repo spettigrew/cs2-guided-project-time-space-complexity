f8bc37ff967f49309aeed89fbd4e10ea13a9fecf
Sara Pettigrew <sarajp.ak@hotmail.com>
2020-12-11 14:05:13 -0700

    Notesand code added to demo from guided lecture

--- src/single-number_demo2.py
+++ src/single-number_demo2.py
@@ -9,5 +9,31 @@ Examples:
 - single_number([5,2,3,2,3]) -> 5
 - single_number([10]) -> 10
 """
+nums = [5,2,3,2,3]
+
 def single_number(nums):
     # Your code here:
+    # U - have numbers that appear twice and I want to find the number that only appears one time.
+    # initialize the first occurance to store the first occuring integer in the nums array  
+    first_occur = []
+    # initializing doubles to hold a double occurance integer in the num array.
+    dups = []
+
+    # iterate through nums array
+    for num in nums:
+        # if this is first occurance of an integer
+        if num not in first_occur:
+            #  put the integer in the first_occur
+            first_occur.append(num)
+        # else if the number is in first occurance it's a duplicate
+        else:
+            # add number to the dups array  
+            dups.append(num)
+    # iterate through the first_occur array
+    for num in first_occur:
+        # if num is not in dups
+        if num not in dups:
+            # then it is the single number
+            return num
+
+print(single_number(nums))
--- src/two-sum_demo1.py
+++ src/two-sum_demo1.py
@@ -1,6 +1,5 @@
 """
-Given an array of integers `nums` and an integer `target`, return the indices
-of the two numbers that add up to the `target`.
+Given an array of integers `nums` and an integer `target`, return the indices of the two numbers that add up to the `target`.
 
 Examples:
 
@@ -18,6 +17,28 @@ Notes:
 def two_sum(nums, target):
     # Your code here
 # U - input: array of nums, number target [0, 1]
+    # set an empty dict
+    num_dict = {}
+
+    for i in range(len(nums)):  # O(n)
+        # keys are the number and the value is the location
+        num_dict[nums[i]] = i
+    
+    # not nested loop - have current num and find it's compliment
+    for i in range(len(nums)):  # O(n) 
+        # - no constants:2n, just O(n)
+        current_num = nums[i]
+        # check if the compliment (a matching number) is in dict to find the sum of the target number. Found the parent
+        compliment = target - current_num
+        
+        #check for dups or if the key exists. If compliment exsits, return its index.      
+        # check that compliment is not in same location i !=
+        if compliment in num_dict and i != num_dict[compliment]:  
+            return [i, num_dict[compliment]]
+
+print(two_sum(nums = [2, 5, 9, 13], target = 7))
+    # total runtime = O(n) - space complexity = O(n)
+    
     # Plan - 2 loops to generate all pairs.
         # loop for each num_1 in nums
         # loop num_2 in nums starting at index + 1
@@ -39,24 +60,6 @@ def two_sum(nums, target):
             #each num:index - insert into the dict
             # find the 2 numbers
                 # for each num, is there a matching number (a compliment) and where is it?
-
-    num_dict = {}
-
-    for i in range(len(nums)):  # O(n)
-        num_dict[nums[i]] = i
-    
-    for i in range(len(nums)):  # O(n) 
-        # - no constants:2n, just O(n)
-        current_num = nums[i]
-        # check if the compliment is in dict
-        compliment = target - current_num
-        
-        #check for dups
-        if compliment in num_dict and i != num_dict[compliment]:  
-            return [i, num_dict[compliment]]
-
-print(two_sum(nums = [2, 5, 9, 13], target = 7))
-
 
     # for i in range(len(nums)-1):
     #     if nums[i] + nums[i+1] == target: 
